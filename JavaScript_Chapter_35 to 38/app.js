8cb8e4d2c3ecbd248f289c66c7051841c4ddd9b4
Fahad.Hassan <fahadfuuast2@gmail.com>
2024-02-05 23:10:35 +0500

    updated

--- JavaScript_Chapter_35 to 38/app.js
+++ JavaScript_Chapter_35 to 38/app.js
@@ -17,3 +17,30 @@
 //     alert(fullName)
 // }
 // userName()
+
+// Question 03
+
+let addNum = () =>{
+    let num1 = +prompt("Write first Number");
+    let num2 = +prompt("Write second Number");
+    num1 = parseInt(num1);
+    num2 = parseInt(num2);
+    return num1 + num2
+}
+
+document.write(addNum())
+
+// Question 04 Calculator
+
+let num1 = +prompt("Enter First Number");
+let num2 = +prompt("Enter Second Number");
+let operator = prompt("Enter Operation","+,-,*,/");
+let num_1 = Math.round(num1);
+let num_2 = Math.round(num2);
+
+if(operator == "+"){
+    let add = (a,b) =>{
+        return a + b;
+    }
+    document.write(`The Sum of Two number ${num1} + ${num2} = ${add(num_1,num_2)}`)
+}
