3a57f3030ff3320d0b1051d77869cb96ee0d4c1e
Fahad.Hassan <fahadfuuast2@gmail.com>
2024-02-05 23:30:42 +0500

    updated

--- JavaScript_Chapter_35 to 38/app.js
+++ JavaScript_Chapter_35 to 38/app.js
@@ -32,36 +32,38 @@
 
 // Question 04 Calculator
 
-let num1 = +prompt("Enter First Number");
-let num2 = +prompt("Enter Second Number");
-let operator = prompt("Enter Operation","+,-,*,/");
-let num_1 = Math.round(num1);
-let num_2 = Math.round(num2);
+// let num1 = +prompt("Enter First Number");
+// let num2 = +prompt("Enter Second Number");
+// let operator = prompt("Enter Operation","+,-,*,/");
+// let num_1 = Math.round(num1);
+// let num_2 = Math.round(num2);
 
-if(operator == "+"){
-    let add = (a,b) =>{
-        return a + b;
-    }
-    document.write(`The Sum of Two Number ${num1} + ${num2} = ${add(num_1,num_2)}`)
-}
-else if(operator == "-"){
-    let sub = (a,b) =>{
-        return a - b
-    }
-    document.write(`Subtraction of Two Number ${num1} - ${num2} = ${sub(num_1,num_2)}`);
-}
-else if(operator == "/"){
-    let div = (a,b) =>{
-        return a/b
-    }
-    document.write(`Division of Two Number ${num1} / ${num2} = ${div(num_1,num_2)}`);
-}
-else if(operator == "*"){
-    let mul = (a,b) =>{
-        return a * b
-    }
-    document.write(`Multiplication of Two Number ${num1} * ${num2} = ${mul(num_1,num_2)}`);
-}
-else{
-    alert(`Should select any of these operators +,-,/,*`)
-}
+// if(operator == "+"){
+//     let add = (a,b) =>{
+//         return a + b;
+//     }
+//     document.write(`The Sum of Two Number ${num1} + ${num2} = ${add(num_1,num_2)}`)
+// }
+// else if(operator == "-"){
+//     let sub = (a,b) =>{
+//         return a - b
+//     }
+//     document.write(`Subtraction of Two Number ${num1} - ${num2} = ${sub(num_1,num_2)}`);
+// }
+// else if(operator == "/"){
+//     let div = (a,b) =>{
+//         return a/b
+//     }
+//     document.write(`Division of Two Number ${num1} / ${num2} = ${div(num_1,num_2)}`);
+// }
+// else if(operator == "*"){
+//     let mul = (a,b) =>{
+//         return a * b
+//     }
+//     document.write(`Multiplication of Two Number ${num1} * ${num2} = ${mul(num_1,num_2)}`);
+// }
+// else{
+//     alert(`Should select any of these operators +,-,/,*`)
+// }
+
+// Question 05
