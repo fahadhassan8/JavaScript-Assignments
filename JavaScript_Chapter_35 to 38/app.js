ed8287cdc14e6cec56d719fdf9a7754c645be81e
Fahad.Hassan <fahadfuuast2@gmail.com>
2024-02-05 21:44:42 +0500

    updated

--- JavaScript_Chapter_35 to 38/app.js
+++ JavaScript_Chapter_35 to 38/app.js
@@ -2,8 +2,18 @@
 
 // Question 01
 
-function currentDate(){
-    var date = new Date();
-    document.write(date)
-}
-currentDate()
+// function currentDate(){
+//     var date = new Date();
+//     document.write(date)
+// }
+// currentDate()
+
+// Question 02
+
+// function userName(){
+//     var firstName = prompt("Enter your First Name");
+//     var lastName = prompt("Enter your Last Name");
+//     var fullName = `Hello ${firstName} ${lastName}`;
+//     alert(fullName)
+// }
+// userName()
