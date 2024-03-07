896583f5d89d4ebb6afe45298241adaf59c55eb6
Fahad.Hassan <fahadfuuast2@gmail.com>
2024-03-07 23:46:33 +0500

    updated

---
+++ JavaScript_Chapter_38 to 42/app.js
@@ -0,0 +1,51 @@
+// Question 01
+
+// function power(a,b){
+//     return Math.pow(a,b); // a is a base num called sub and b is power called sup
+// }
+// let result = power(2,3);
+// console.log(result);
+
+// let num = 25;
+// let sqR = Math.sqrt(num) // squareRoot will be 5
+// console.log(sqR)
+
+// Question 02
+
+// function calculateS(a,b,c){
+//     return (a + b + c)/2
+// }
+// function calculateTraingleArea(a,b,c){
+//     let S = calculateS(a,b,c);
+//     let area = Math.sqrt(S * (S - a) * (S - b) * (S - c));
+//     return area;
+// }
+// let a = 5;
+// let b = 5;
+// let c = 6;
+// let area = calculateTraingleArea(a,b,c);
+// console.log(area);
+
+// Question 03
+
+// function calculateAverage(subject1, subject2, subject3){
+//     return (subject1 + subject2 + subject3)/3
+// }
+// function calculatePercentage(totalMarks, obtainedMarks){
+//     return (obtainedMarks / totalMarks) * 100;
+// }
+// function mainFunction(subject1, subject2, subject3){
+//     let average = calculateAverage(subject1, subject2, subject3);
+//     let obtainedMarks = subject1 + subject2 + subject3;
+//     let totalMarks = 300;
+//     let percentage = calculatePercentage(totalMarks, obtainedMarks);
+//     console.log("Average: " + average);
+//     console.log("Percentage: " + percentage);
+// }
+// mainFunction(70,50,60)
+
+// Question 04
+
+function custom(){
+    
+}
