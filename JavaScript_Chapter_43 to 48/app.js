df7137bef1d9ad6e34d0ded93c0677e19512cabd
Fahad.Hassan <fahadfuuast2@gmail.com>
2024-03-12 22:26:56 +0500

    update

--- JavaScript_Chapter_43 to 48/app.js
+++ JavaScript_Chapter_43 to 48/app.js
@@ -1,6 +1,6 @@
 // Question 01
 
-document.querySelector('.myLink').addEventListener('click', function(event){
+document.querySelector('#myLink').addEventListener('click', function(event){
     event.preventDefault();
     alert('You Clicked on me')
 });
