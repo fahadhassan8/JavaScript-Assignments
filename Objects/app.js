9e7a48318b1a63cf966465af76bfb14e98fd75a0
Fahad.Hassan <fahadfuuast2@gmail.com>
2024-04-01 21:50:19 +0500

    updated

---
+++ Objects/app.js
@@ -0,0 +1,39 @@
+// // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> OBJECTS <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+// // QUESTION 01
+
+// // var itemsArray = [
+// // {name:”juice”,price:”50”, quantity:”3”},
+// // {name:”cookie”,price:”30”, quantity:”9”},
+// // {name:”shirt”,price:”880”, quantity:”1”},
+// // {name:”pen”,price:”100”, quantity:”2”}];
+
+// var itemsArray = [
+//     {
+//         name:"Juice",
+//         price:50,
+//         quantity:3
+//     },
+//     {
+//         name:"Cookie",
+//         price:30,
+//         quantity:9
+//     },
+//     {
+//         name:"Shirt",
+//         price:880,
+//         quantity:1
+//     },
+//     {
+//         name:"Pen",
+//         price:100,
+//         quantity:2
+//     }
+// ]
+// let totalPriceOfAllItems = 0;
+// for(let i = 0; i < itemsArray.length; i++){
+//     let totalPriceOfItem = itemsArray[i].price * itemsArray[i].quantity;
+//     totalPriceOfAllItems += totalPriceOfItem
+//     console.log(`Total Price of Items ${totalPriceOfItem}`);
+// }
+// console.log(`Total Price of All Items ${totalPriceOfAllItems}`);
