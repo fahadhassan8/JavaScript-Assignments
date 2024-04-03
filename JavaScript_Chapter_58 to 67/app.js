a4cb7d36bba52653bb5581b3b4d18d657f30fe77
Fahad.Hassan <fahadfuuast2@gmail.com>
2024-04-03 23:29:33 +0500

    assignment complete

--- JavaScript_Chapter_58 to 67/app.js
+++ JavaScript_Chapter_58 to 67/app.js
@@ -27,3 +27,36 @@ lastName.value = "Bank";
 let email = document.getElementById("email");
 
 email.value = "alexbank@example.com";
+
+
+// Question 02
+
+let formContentNode = document.getElementById('form-content');
+
+console.log('Node Type of element having id form-content', formContentNode.nodeType);
+
+let lastNameNode = document.getElementById('lastName');
+
+console.log('Node Type of element having id lastName', lastNameNode.nodeType);
+console.log('Child Node of element having id lastName', lastNameNode.childNodes);
+
+lastNameNode.innerHTML = "Khan";
+console.log('Last Name: ',lastNameNode);
+
+let mainContentNode = document.getElementById('main-content');
+console.log('First Child of element having id main-content',mainContentNode.firstChild); 
+console.log('Last Child of element having id main-content',mainContentNode.lastChild); 
+
+let lastNameNextSibling = lastNameNode.nextSibling;
+
+console.log('Next Siblings of element having id lastName', lastNameNextSibling);
+
+let lastNamePreviousSibling = lastNameNode.previousSibling;
+
+console.log('Previous Siblings of element having id lastName', lastNamePreviousSibling);
+
+let emailNode = document.getElementById("email");
+console.log('Node Type of element having id email', emailNode);
+
+let emailParentNode = emailNode.parentNode;
+console.log('Parent Node of element having id email',emailParentNode);
